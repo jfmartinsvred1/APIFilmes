13d5f79ef8713b2b20f9c148ae1fd5c07faac030
JOAO <JOAO@DESKTOP-NCVO2P5>
2023-08-12 19:19:12 -0300

    Adicionar arquivos de projeto.

---
+++ ApiFilmes/Controllers/FilmeController.cs
@@ -0,0 +1,36 @@
+﻿
+using ApiFilmes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiFilmes.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class FilmeController:ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody]Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(
+            nameof(RecuperaFilmePorID),
+            new {id = filme.Id},
+            filme);
+    }
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery]int skip=0, [FromQuery]int take=15)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorID(int id)
+    {
+        var result =filmes.FirstOrDefault(filme => filme.Id == id);
+        if(result == null) return NotFound();
+        return Ok(result);
+        
+    }
+
+}
---
+++ ApiFilmes/Data/FilmesContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiFilmes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFilmes.Data;
+
+public class FilmesContext:DbContext
+{
+    public FilmesContext(DbContextOptions<FilmesContext> opts) 
+        : base(opts) 
+    {
+
+    }
+    public DbSet<Filme> Filmes { get; set; }
+
+    
+}
